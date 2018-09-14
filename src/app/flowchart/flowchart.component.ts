56247816cb2fecd969f0422cecd9c99e607aaa34
huangchunshui <chunshui_h@163.com>
2018-09-14 22:57:56 +0800

    修复粘贴功能bug, 调整粘贴元素的位置

--- src/app/flowchart/flowchart.component.ts
+++ src/app/flowchart/flowchart.component.ts
@@ -1,4 +1,4 @@
-import {Component, NgZone, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, NgZone, OnInit} from '@angular/core';
 @Component({
   templateUrl: 'flowchart.component.html',
   styleUrls: ['flowchart.component.less']
@@ -27,14 +27,17 @@ export class FlowchartComponent implements OnInit {
     maxConnections: -1,	// 设置连接点最多可以连接几条线
     connectorOverlays: [['Arrow', { width: 5, length: 5, location: 1 }]]
   };
-  constructor( private zone: NgZone) {
+  constructor( private zone: NgZone, public changeDetectorRef: ChangeDetectorRef) {
 
   }
 
   right_click(e, node) {   // 右键， 触发显示菜单
     e.preventDefault();
     e.stopPropagation();    // stop global event
+    const l = e.clientX - 215;
+    const r = e.clientY - 50;
     this.right_menu_style = {left: e.clientX + 'px', top: e.clientY + 'px'};
+    console.log(this.right_menu_style);
     this.cur_node = node;
     this.showMenu = true;
   }
@@ -65,40 +68,42 @@ export class FlowchartComponent implements OnInit {
       id : this.uuid(),
       name : '',
       model_id: this.cur_node.model_id,
-      style: {'top': this.cur_node.style.top , 'left': this.cur_node.style.left}};
+      style: {'top': this.right_menu_style.top , 'left': this.right_menu_style.left}};
   }
 
   pasteNode(e) {
     this.showMenu = false;
+    this.right_menu_style.left = parseInt(this.right_menu_style.left.split('px')[0], 10) - 215 + 'px';
+    this.right_menu_style.top = parseInt(this.right_menu_style.top.split('px')[0], 10) - 50 + 'px';
     this.copy_node.style = this.right_menu_style;
     this.nodes[this.copy_node.model_id].push(this.copy_node);
-    this.zone.run(() => {});
-    const id = this.copy_node.id;
-    jsPlumb.addEndpoint(id, { anchor: 'Right'}, this.hollowCircle);
-    jsPlumb.addEndpoint(id, { anchor: 'Left' }, this.hollowCircle);
-    jsPlumb.addEndpoint(id, { anchor: 'Top' }, this.hollowCircle);
-    jsPlumb.addEndpoint(id, { anchor: 'Bottom' }, this.hollowCircle);
-    jsPlumb.draggable(id);
+    this.changeDetectorRef.detectChanges();
+      const id = this.copy_node.id;
+      jsPlumb.addEndpoint(id, { anchor: 'Right'}, this.hollowCircle);
+      jsPlumb.addEndpoint(id, { anchor: 'Left' }, this.hollowCircle);
+      jsPlumb.addEndpoint(id, { anchor: 'Top' }, this.hollowCircle);
+      jsPlumb.addEndpoint(id, { anchor: 'Bottom' }, this.hollowCircle);
+      jsPlumb.draggable(id);
 
-    // 1.3 ?????draggable?resizable
-    $('#' + id).draggable({
-      containment: 'parent',
-      start: function () {
-        jsPlumb.repaintEverything();
-      },
-      drag:  (event, _ui) => {
-        jsPlumb.repaintEverything();
-      },
-      stop: function () {
-        jsPlumb.repaintEverything();
-      }
-    });
+      // 1.3 ?????draggable?resizable
+      $('#' + id).draggable({
+        containment: 'parent',
+        start: function () {
+          jsPlumb.repaintEverything();
+        },
+        drag:  (event, _ui) => {
+          jsPlumb.repaintEverything();
+        },
+        stop: function () {
+          jsPlumb.repaintEverything();
+        }
+      });
 
-    $('#' + id).resizable({
-      stop: function( event, ui) {
-        jsPlumb.repaintEverything();
-      }
-    });
+      $('#' + id).resizable({
+        stop: function( event, ui) {
+          jsPlumb.repaintEverything();
+        }
+      });
   }
 
   ngOnInit() {
